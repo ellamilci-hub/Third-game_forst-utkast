e82b1f0493e228d8e87b5030427d04217e210c8a
ellamilci-hub <ella.milci@gmail.com>
2025-12-17 14:24:19 +0100

    start of tracking
    unstable - moving tiles and character movement

---
+++ Assets/Scenes/character/sunflower/movement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+    private bool grounded;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
+
+        //flip character when moving left and right
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+        if (Input.GetMouseButtonDown(0))
+            body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
+        
+    }
+}
